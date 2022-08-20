033f2ac0d7d67d5971f7a67de5dc34c16300e256
a-skib <111177248+a-skib@users.noreply.github.com>
2022-08-20 14:14:52 +0200

    ola

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,33 +2,16 @@ from pages.base_page import BasePage
 
 
 class Dashboard(BasePage):
-Scouts_Panel_xpath = "//*[text()=’Scouts Panel’]"
-Scouts_Panel_xpath = "//*[@id=’__next’]/form/div/div[1]/h5"
-Scouts_Panel_xpath = "//*[contains(@class, ‘MuiTypography-h5’)]"
-
-Login_xpath= "//*[@id=’login’]"
-Login_xpath= "//label[text()=’Login’]"
-Login_xpath= "//*[@name = ‘login’ and @id = ‘login’]"
-
-
-Password_xpath = "//*[@id=’password’]"
-Password_xpath = "//label[text()=’Password’]"
-Password_xpath = "//*[@name = ‘password’ and @id = ‘password’]"
-
-Remind_password_xpath = "//*[@id=’__next’]/form/div/div[1]/a"
-Remind_password_xpath = "//*[text()=’Remind password’]"
-Remind_password_xpath = "//a[contains (@class, ‘MuiTypography’)]"
-
-English_xpath = "//*[@role= ‘option’ and @tabindex=’0’]"
-English_xpath = "//li[contains(@class, ‘MuiButtonBase-root’) and @data-value=’en’]"
-English_xpath = "//li[contains(@role, ‘option’) and @data-value=’en’]"
-
-Polski_xpath = "//div[contains(@class, ‘MuiSelect’)]"
-Polski_xpath = "//div[contains(@role, ‘button’)]"
-Polski_xpath = "//*[@role= ‘button’ and @tabindex=’0’]"
-
-Zaloguj_xpath = "//*[text()=’Zaloguj’]"
-Zaloguj_xpath = "//span[contains(@class, ‘MuiButton’)]"
-Zaloguj_xpath = "//*[contains(@class, ‘MuiButton’) and text()=’Zaloguj’]"
+    main_page_xpath = "// span[contains(@class , 'MuiTypography') and text()= 'Main page']"
+    players_xpath = "//*[@class= 'MuiListItemText-root jss29 jss31']"
+    polski_xpath = "//*[text() = 'Polski']"
+    sign_out_xpath = "//*[text() = 'Sign out']"
+    players_count_xpath = "//*[text()= 'Players count']"
+    matches_count_xpath = "//*[text()= 'Matches count']"
+    reports_count_xpath = "//*[text()= 'Reports count']"
+    events_count_xpath = "//*[text()= 'Events count']"
+    scouts_pane_xpath = "//h2[text()= 'Scouts Panel']"
+    shortcuts_xpath = "//span[text()= 'Add player']"
+    activity_xpath = "//*[@href='/en/players/62f2bce6159aa3d4fa18f4b2/edit']"
 pass
 
